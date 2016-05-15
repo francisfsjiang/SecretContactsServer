b74a95cdee55328d24d3190f2996f25fa67c8b8b
Neveralso <neveralso@gmail.com>
2016-05-16 00:43:55 +0800

    commit

--- secret_contacts/schedule_task.py
+++ secret_contacts/schedule_task.py
@@ -2,6 +2,7 @@ import threading
 import time
 import pymongo
 import pymongo.errors
+import hashlib
 
 from json import dumps
 
@@ -20,9 +21,11 @@ class TaskThread(threading.Thread):
                         [doc["phone"], doc["mark_time"]]
                     )
                 time_now = int(time.time())
+                md5 = hashlib.md5(str(content_arr).encode()).hexdigest()
                 json = {
                     "update_time": time_now,
-                    "content": dumps(content_arr)
+                    "content": dumps(content_arr),
+                    "md5": md5
                 }
                 db.harassing_cache.save(json)
                 print(json)
